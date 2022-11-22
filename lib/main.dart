346faf333a1f8819a6077b7934b2a60d10d6633b
DiseirangCHHEUN <111745190+Diseirang@users.noreply.github.com>
2022-11-22 17:49:29 +0700

    Edited

--- lib/main.dart
+++ lib/main.dart
@@ -184,7 +184,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
-            const CustomExpansion(),
+            //const CustomExpansion(),
             if (!islandscape)
               SizedBox(
                 height: (mediaQuery.size.height -
